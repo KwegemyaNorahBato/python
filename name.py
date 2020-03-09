db5e52bad43f5149798df45ef64f075e5a1e368d
KwegemyaNorahBato <61685729+KwegemyaNorahBato@users.noreply.github.com>
2020-03-09 05:42:18 +0300

    my name py example
    my name py example

---
+++ name.py
@@ -0,0 +1,5 @@
+def personal_details():
+    name, points, combination, university = "Norah", 19, "DEG", "Makerere"
+    print("Name: {}\nPoints: {}\nCombination: {}\nUniversity: {}\nAddress: {}".format(name, points,combination, university))
+
+personal_details()
